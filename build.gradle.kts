642178c0e58cbddc0b35a40ed7bb694c9a88164a
rajeev-k47 <rajeevkumawat2007@outlook.com>
2026-06-09 05:16:00 +0530

    <Fix: */*>?

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
 }
 
 dependencies {
-    api("com.github.ktorite:ktorite:1.0.0")
+    api("com.github.ktorite:ktorite-core:1.0.0")
     api("io.ktor:ktor-server-core:3.5.0")
     api("io.ktor:ktor-server-thymeleaf:3.5.0")
     api("io.ktor:ktor-server-sessions:3.5.0")
